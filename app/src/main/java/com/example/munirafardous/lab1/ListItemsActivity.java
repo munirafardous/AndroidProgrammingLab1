74acd4f1217c7b85f19fdd883f332d5ba630fe7a
MuniraFardousNahin <nahi0003@algonquinlive.com>
2016-09-27 20:09:52 -0400

    initial commit

--- app/src/main/java/com/example/munirafardous/lab1/ListItemsActivity.java
+++ app/src/main/java/com/example/munirafardous/lab1/ListItemsActivity.java
@@ -9,5 +9,8 @@ public class ListItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        //
+        //
+        //
     }
 }
